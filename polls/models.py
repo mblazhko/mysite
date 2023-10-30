50d641c6561c3929969f4b047de809cdf1eb2d5e
neostyle88 <makssimm1@gmail.com>
2023-10-30 17:18:24 +0100

    add poll & answer models

--- polls/models.py
+++ polls/models.py
@@ -4,8 +4,13 @@ from django.db import models
 from django.utils import timezone
 
 
+class Poll(models.Model):
+    poll_name = models.CharField(max_length=200)
+
+
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=255)
     pub_date = models.DateTimeField("date published")
 
     def __str__(self) -> str:
@@ -18,7 +23,14 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
 
     def __str__(self) -> str:
         return self.choice_text
+
+
+class Answer(models.Model):
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.answer_text
