ad12ca0aa6c830a857c6f88b82373a1bdec1db0e
neostyle88 <makssimm1@gmail.com>
2023-11-01 20:42:57 +0100

    make it look prettier

--- polls/urls.py
+++ polls/urls.py
@@ -4,10 +4,11 @@ from django.views.decorators.cache import cache_page
 from . import views
 
 urlpatterns = [
-    path("", cache_page(60 * 30)(views.IndexView.as_view()), name="index"),
+    path("", cache_page(60 * 10)(views.IndexView.as_view()), name="index"),
     path("<int:pk>/", views.poll_detail, name="poll-detail"),
-    path("<int:pk>/results/", cache_page(60 * 30)(views.ResultsView.as_view()), name="poll-results"),
-    path("<int:poll_id>/vote/", views.vote, name="vote"),
+    path(
+        "<int:pk>/results/", views.ResultsView.as_view(), name="poll-results"
+    ),
 ]
 
 app_name = "polls"
