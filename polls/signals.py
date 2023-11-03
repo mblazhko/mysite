41b2eb9d0981638d76af0db915b5225bc4ce7357
neostyle88 <makssimm1@gmail.com>
2023-11-03 15:54:11 +0100

    add setting new popular polls cache if some changes occurred

--- polls/signals.py
+++ polls/signals.py
@@ -1,8 +1,9 @@
 from django.core.cache import cache
-from django.db.models.signals import post_save, pre_delete
+from django.db import models
+from django.db.models.signals import post_save, pre_delete, post_delete
 from django.dispatch import receiver
 
-from polls.models import Answer, Question
+from polls.models import Answer, Question, Poll
 
 
 @receiver(signal=pre_delete, sender=Question)
@@ -17,3 +18,16 @@ def invalidate_poll_cache(**kwargs) -> None:
     if isinstance(instance, Question):
         cache_key = f"poll_{instance.poll.slug}"
     cache.delete(cache_key)
+
+
+@receiver(signal=post_delete, sender=Poll)
+@receiver(signal=post_save, sender=Answer)
+def invalidate_popular_poll_cache(**kwargs) -> None:
+    popular_polls_before = cache.get("popular_polls")
+    popular_polls_after = Poll.objects.annotate(
+        num_answers=models.Count("question__choice__answer")
+    ).order_by("-num_answers")[:10]
+
+    if popular_polls_before != popular_polls_after:
+        cache.delete("popular_polls")
+        cache.set("popular_polls", popular_polls_after)
