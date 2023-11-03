be1d2c7647b4380ac515344cae8275a11816c48a
neostyle88 <makssimm1@gmail.com>
2023-11-03 19:05:54 +0100

    add permission IsAuthenticated as default to api

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,6 +105,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PERMISSION_CLASSES': [
+            'rest_framework.permissions.IsAuthenticated',
+        ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
