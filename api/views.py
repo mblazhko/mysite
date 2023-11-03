23da39fd2107202c26641575af6c43971aa11608
neostyle88 <makssimm1@gmail.com>
2023-11-03 18:36:42 +0100

    add an add_question & add_choice methods

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, mixins
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from polls.models import Poll, Question, Choice, Answer
 from api.serializers import (
@@ -29,23 +30,48 @@ class PollViewSet(
             return PollListSerializer
         if self.action == "retrieve":
             return PollDetailSerializer
-        if self.action == "create":
-            return PollDetailSerializer
+        if self.action == "add_question":
+            return QuestionSerializer
         return PollSerializer
+
+    @action(detail=True, methods=['post'])
+    def add_question(self, request, pk=None):
+        poll = self.get_object()
+        question_text = request.data.get('question_text')
+        if question_text:
+            question = Question.objects.create(poll=poll,
+                                               question_text=question_text)
+            return Response(QuestionSerializer(question).data, status=201)
+        else:
+            return Response({"error": "Question text is required."},
+                            status=400)
 
 
 class QuestionViewSet(
     viewsets.GenericViewSet,
     mixins.RetrieveModelMixin,
-    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
 ):
-    queryset = Question.objects.select_related("choice_set")
+    queryset = Question.objects.select_related("poll")
     serializer_class = QuestionSerializer
 
     def get_serializer_class(self):
-        if self.action == "retrieve" or self.action == "create":
+        if self.action == "retrieve":
             return QuestionDetailSerializer
+        if self.action == "add_choice":
+            return ChoiceSerializer
         return QuestionSerializer
+
+    @action(detail=True, methods=['post'])
+    def add_choice(self, request, pk=None):
+        question = self.get_object()
+        choice_text = request.data.get('choice_text')
+        if choice_text:
+            choice = Choice.objects.create(question=question,
+                                           choice_text=choice_text)
+            return Response(ChoiceSerializer(choice).data, status=201)
+        else:
+            return Response({"error": "Choice text is required."}, status=400)
 
 
 class ChoiceViewSet(
@@ -53,7 +79,7 @@ class ChoiceViewSet(
     mixins.RetrieveModelMixin,
     mixins.CreateModelMixin,
 ):
-    queryset = Choice.objects.all()
+    queryset = Choice.objects.select_related("question__poll")
     serializer_class = ChoiceSerializer
 
 
@@ -61,8 +87,8 @@ class AnswerViewSet(
     viewsets.GenericViewSet,
     mixins.CreateModelMixin,
 ):
-    queryset = Answer.objects.all()
+    queryset = Answer.objects.none()
     serializer_class = AnswerSerializer
 
     def perform_create(self, serializer) -> None:
-        serializer.save(user=self.request.user)
+        serializer.save(owner=self.request.user)
