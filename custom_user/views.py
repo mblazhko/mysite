30aedd55089e64d3037837782233cd3ffc566d08
neostyle88 <makssimm1@gmail.com>
2023-11-04 16:04:03 +0100

    optimize user polls query

--- custom_user/views.py
+++ custom_user/views.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+from polls.models import Poll
+
 
 @login_required
 def user_profile(request):
-    return render(request, 'user/profile.html')
+    user = request.user
+    user_polls = Poll.objects.filter(owner=user).select_related('owner')
+    return render(request, 'user/profile.html', context={"polls": user_polls})
 
 
 @login_required
