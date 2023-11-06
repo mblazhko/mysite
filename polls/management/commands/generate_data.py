d59c7f682170abe8e1e179323e6ca2e283192f8d
neostyle88 <makssimm1@gmail.com>
2023-11-06 14:02:44 +0100

    add assign user to polls and answers

--- polls/management/commands/generate_data.py
+++ polls/management/commands/generate_data.py
@@ -7,7 +7,10 @@ from polls.models import Poll, Question, Choice, Answer
 class Command(BaseCommand):
     help = "Generate random data and fill the database."
 
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **kwargs) -> None:
+
+        self.stdout.write("Start generating random data")
+
         superuser = get_user_model().objects.create_superuser(
             email="admin@admin.com",
             password="V!5WucFeReMQ7fg",
@@ -15,14 +18,18 @@ class Command(BaseCommand):
             last_name="TestLastName",
         )
 
-        # generate 100 users
-        for i in range(1, 1001):
+        self.stdout.write(f"Superuser {superuser.email} created")
+
+        # generate 20 users
+        for i in range(1, 21):
             get_user_model().objects.create_user(
                 email=f"user_{i}@user.com",
                 password=f"V!5WucFeReMQ7fg{i}",
                 first_name=f"FirstName{i}",
                 last_name=f"LastName{i}",
             )
+
+        self.stdout.write("20 users created")
 
         # Generate 1000 Polls
         for i in range(1, 1001):
@@ -31,6 +38,8 @@ class Command(BaseCommand):
                 poll_description=f"Description for Poll {i}",
                 owner=superuser,
             )
+
+            self.stdout.write(f"Poll {poll.poll_name} created")
 
             # Generate 4 Questions for each Poll
             for j in range(1, 5):
@@ -45,6 +54,10 @@ class Command(BaseCommand):
                         choice_text=f"Choice {k} for Question {j} in Poll {i}",
                     )
 
+            self.stdout.write(
+                f"Questions and Choices for {poll.poll_name} created"
+            )
+
         users = get_user_model().objects.all()
         questions = Question.objects.all()
 
@@ -52,6 +65,8 @@ class Command(BaseCommand):
             for question in questions:
                 random_choice = random.choice(question.choice_set.all())
                 Answer.objects.create(owner=user, choice=random_choice)
+
+            self.stdout.write(f"User {user.email} voted")
 
         self.stdout.write(
             self.style.SUCCESS("Successfully generated random data.")
