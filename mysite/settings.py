17e1bf5d9c0602e3e33b2d0b51df2c00e8b8ecbb
neostyle88 <makssimm1@gmail.com>
2023-11-06 18:08:46 +0100

    remove drf browsable api

--- mysite/settings.py
+++ mysite/settings.py
@@ -145,6 +145,9 @@ CACHES = {
 
 # api settings
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+            'rest_framework.renderers.JSONRenderer',
+        ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
