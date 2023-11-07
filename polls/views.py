f72508daad145efd58c0e4bfb70df8ee67b510e7
neostyle88 <makssimm1@gmail.com>
2023-11-07 11:38:38 +0100

    optimize query for poll detail view

--- polls/views.py
+++ polls/views.py
@@ -80,11 +80,13 @@ class PollDetailView(LoginRequiredMixin, generic.DetailView):
 
     model = Poll
     template_name = "polls/poll_detail.html"
-    context_object_name = "poll"
 
     def get_context_data(self, **kwargs) -> dict:
         """Get data if user already has voted"""
         context = super().get_context_data(**kwargs)
+        context["questions"] = Question.objects.prefetch_related(
+            "choice_set__answer_set"
+        ).filter(poll=self.object)
         context["has_voted"] = Answer.objects.filter(
             owner=self.request.user, choice__question__poll=self.object
         ).exists()
